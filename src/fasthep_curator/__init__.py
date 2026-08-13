85b2bf9c544032bd0e8c3ae92c1b4557c1afec73
kreczko <lkreczko@googlemail.com>
2026-08-13 11:16:39 +0100

    feat: expose .api under main package __all__

--- src/fasthep_curator/__init__.py
+++ src/fasthep_curator/__init__.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
+from fasthep_curator import api as api
+
 try:
     from ._version import version as __version__
 except ModuleNotFoundError:
     __version__ = "0+unknown"
+
+__all__ = ["__version__", "api"]
